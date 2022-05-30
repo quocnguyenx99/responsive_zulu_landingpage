0b0f4d121ecc335bc628c62d644cd7416d79c1b5
unknown <nhquoc99@gmail.com>
2022-05-30 17:05:51 +0700

    upload source code

---
+++ app.js
@@ -0,0 +1,17 @@
+function handleToggleMenu() {
+    const menuToggle = document.querySelector('.menu-toggle')
+    const menu = document.querySelector('.header__menu')
+    if(!menuToggle || !menu ) return;
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('is-active')
+    })
+    
+    document.addEventListener('click', (e) => {
+        if(!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+            menu.classList.toggle('is-active')
+        }
+    })
+}
+
+handleToggleMenu();
+
